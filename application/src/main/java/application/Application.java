6e4693fa92069f7086defc97a77c09881d6a3516
Jake Bles <jacob.bles@broadcom.com>
2020-04-13 17:28:03 -0500

    2 microservices talking to each other

--- application/src/main/java/application/Application.java
+++ application/src/main/java/application/Application.java
@@ -1,14 +1,16 @@
 package application;
-
 import MessageOfDay.MessageOfDay;
 import org.json.simple.parser.JSONParser;
 
 public class Application {
     public static void main(String[] args) {
         System.out.println("Goodbye cruel world ...");
-        MessageOfDay.message();
+
+
 
         JSONParser json = new JSONParser();
         System.out.println(json);
+
+        MessageOfDay.message();
     }
 }
